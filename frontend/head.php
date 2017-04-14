1ab9087f39b46eefe0d9da221b434c271848ed57
mitchelawesome <mitchel@austinmedia.nl>
2017-04-14 10:52:59 +0200

    added filters page

--- frontend/head.php
+++ frontend/head.php
@@ -9,7 +9,7 @@
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-sweetalert/1.0.1/sweetalert.min.css" rel="stylesheet">
  	<link rel="stylesheet" href="css/styles.css?v=1.0">
-
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
 
   <!--[if lt IE 9]>
---
+++ frontend/views/filters.view.php
@@ -0,0 +1,91 @@
+
+<?php include('snippets/header.php'); ?>
+
+<div class="container-fluid top_search text-center">
+
+	<input type="" name="">
+	<button type="submit">GO</button>
+
+</div>
+<div class="container">
+
+	<div class="row" style="margin-top:40px">
+		<div class="col-md-12">
+			<h1>
+				Pick a Local Experience
+			</h1>
+		</div>
+	</div>
+
+	<div class="row" style="margin-top:40px">
+
+		<div class="col-md-3" style="margin-bottom:20px;">
+			<div style="background:url(https://withlocals-com-res.cloudinary.com/image/upload/w_653,h_435,c_fill,g_auto,q_auto,f_auto/5f4b9c2d2c7318ff9ce22338410b9155) 50% 50% no-repeat blue; background-size:cover; width:100%; height:150px;">
+				<a href="#" class="overlay">
+					<div class="display-table">
+						<div class="display-table">
+							<div class="display-table-cell text-center text-middle">
+								<i class="glyphicon glyphicon-heart loveit"></i>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<h3>Local Food Tour</h3>
+			<p>Private Guide</p>
+		</div>
+
+		<div class="col-md-3" style="margin-bottom:20px;">
+			<div style="background:url(https://withlocals-com-res.cloudinary.com/image/upload/w_660,h_440,c_fill,g_auto,q_auto,f_auto/0dee9e3780ec0ea3e73c8a5716d46dd4) 50% 50% no-repeat blue; background-size:cover; width:100%; height:150px;">
+				<a href="#" class="overlay">
+					<div class="display-table">
+						<div class="display-table">
+							<div class="display-table-cell text-center text-middle">
+								<i class="glyphicon glyphicon-heart loveit"></i>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<h3>Photo Tour</h3>
+			<p>Private Guide</p>
+		</div>
+
+		<div class="col-md-3" style="margin-bottom:20px;">
+			<div style="background:url(https://withlocals-com-res.cloudinary.com/image/upload/w_653,h_435,c_fill,g_auto,q_auto,f_auto/73e05b57aec6cfc190e83ecbb6bf4137) 50% 50% no-repeat blue; background-size:cover; width:100%; height:150px;">
+				<a href="#" class="overlay">
+					<div class="display-table">
+						<div class="display-table">
+							<div class="display-table-cell text-center text-middle">
+								<i class="glyphicon glyphicon-heart loveit"></i>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<h3>Night Tour</h3>
+			<p>Private Guide</p>
+		</div>
+
+		<div class="col-md-3" style="margin-bottom:20px;">
+			<div style="background:url(https://withlocals-com-res.cloudinary.com/image/upload/w_653,h_435,c_fill,g_auto,q_auto,f_auto/8459fe834d9a0ef857ca0ae519ccd359) 50% 50% no-repeat blue; background-size:cover; width:100%; height:150px;">
+				<a href="#" class="overlay">
+					<div class="display-table">
+						<div class="display-table">
+							<div class="display-table-cell text-center text-middle">
+								<i class="glyphicon glyphicon-heart loveit"></i>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<h3>Off the beaten track</h3>
+			<p>Private Guide</p>
+		</div>
+
+
+
+		
+	</div>
+	
+</div>
